929a5906efe74f212979e6a2e1037dc7f08936ff
Alessandro <holybuz@gmail.com>
2019-06-17 19:22:21 +0200

    DEFINIZIONE STRUTTURA VIEWS E MODELS DI DASHBOARD

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+class Valuta(models.Model):
+    sigla = None
+    cambio = None
+    nome = None
+
+    def __unicode__(self):
+        None
+
+    def converti_cambio(self,valuta):
+        None
+
+
+class Conto(models.Model):
+    tipo_valuta = None
+    importo = None
+    # id = None
+    # wallet_id = None
+
+    def __unicode__(self):
+        None
+
+    def calcola_totale_conto(self,valuta):
+        None
+
+    def aggiungi_importo(self,importo):
+        None
+
+    def rimuovi_importo(self,importo):
+        None
+
+    def get_url(self):
+        None
+
+    @staticmethod
+    def crea_conto(utente,valuta,importo):
+        None
+
+    @staticmethod
+    def rimuovi_conto(utente, valuta):
+        None
+
+
+class Wallet(models.Model):
+    id = None
+    user_id = None
+    conti = None
+    cambio_selezionato = None
+
+    def __unicode__(self):
+        None
+
+    def calcola_totale_wallet(self):
+        None
+
+    def aggiungi_conto(self,valuta,importo):
+        None
+
+    def rimuovi_conto(self,valuta):
+        None
+
+    def modifica_cambio_selezionato(self,valuta):
+        None
+
+    def avvia_transazione(self,utente_destinatario,valuta,importo):
+        None
