af94c5767af2c0e68499a6513895a3b5c7448f51
binomac <lucacambon@gmail.com>
2019-06-17 18:00:57 +0200

    Divisione progetto in 3 componenti

--- smartwallet/settings.py
+++ smartwallet/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Dashboard.apps.DashboardConfig',
+    'Transazioni.apps.TransazioniConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
