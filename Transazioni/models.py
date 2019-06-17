2ace175230b4046a2232350532dd313181819a4e
Alessandro <holybuz@gmail.com>
2019-06-17 19:23:24 +0200

    DEFINIZIONE STRUTTURA VIEWS E MODELS DI TRANSAZIONI

--- Transazioni/models.py
+++ Transazioni/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Transazione(models.Model):
+    id = None
+    data = None
+    wallet_mandante = None
+    wallet_destinatario = None
+    valuta = None
+    importo = None
+    causale = None
+
+    def __unicode__(self):
+        None
+
+    def get_id(self):
+        None
+
+    @staticmethod
+    def crea_transazione(mandante, destinatario, valuta, importo, causale):
+        None
