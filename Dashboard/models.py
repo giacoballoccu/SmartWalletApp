2810e9de90f2335d30439fcc5fbe857b792a470e
giacoballoccu <giacoballoccu@live.it>
2019-06-18 11:02:24 +0200

    Scheletro models

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,23 +1,27 @@
 from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth.models import User
 
 
 class Valuta(models.Model):
-    sigla = None
-    cambio = None
-    nome = None
+    sigla = models.CharField(max_length=5,  null=False, unique=True)
+    cambio = models.IntegerField(null=False)
+    nome = models.CharField(max_length=25, null=False)
+
+    class Meta:
+        ordering = ('sigla',)
 
     def __unicode__(self):
         None
 
     def converti_cambio(self,valuta):
-        None
+        self.cambio = valuta.cambio/self.cambio
 
 
 class Conto(models.Model):
-    tipo_valuta = None
-    importo = None
-    # id = None
-    # wallet_id = None
+    tipo_valuta = models.ForeignKey(Valuta, on_delete=models.CASCADE, null=False)
+    importo = models.IntegerField(default=0)
 
     def __unicode__(self):
         None
@@ -43,11 +47,12 @@ class Conto(models.Model):
         None
 
 
+
 class Wallet(models.Model):
-    id = None
-    user_id = None
-    conti = None
-    cambio_selezionato = None
+    wallet_id = models.CharField(max_length=35, null=False, unique=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    conti = models.ForeignKey(Conto, on_delete=models.CASCADE)
+    cambio_selezionato = models.ForeignKey(Valuta)
 
     def __unicode__(self):
         None
@@ -66,3 +71,14 @@ class Wallet(models.Model):
 
     def avvia_transazione(self,utente_destinatario,valuta,importo):
         None
+
+
+class Transazione(models.Model):
+    id_transazione = models.CharField(max_length=32, null=False, unique=True)
+    data = models.DateTimeField(default=timezone.now(), null=False)
+    input_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, null=False)
+    output_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, null=False)
+    cryptocurrency = models.CharField(max=5, null=False)
+    quantita = models.IntegerField(null=False)
+
+
