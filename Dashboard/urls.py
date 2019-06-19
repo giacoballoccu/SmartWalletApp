923f0ee1a216c5ad31bb8236576e339eb63b7ef2
GiuliaAriu <giuulia.ariu@gmail.com>
2019-06-19 19:31:07 +0200

    Urls per dashboard

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-
+    path('', views.dashboard, name='dashboard'),
+    path('aggiungi_conto/', views.aggiungi_conto, name='aggiungi_conto'),
+    #path('modifica_conto/<string:valuta_type>/', views.modifica_importo, name='modifica_importo_conto'),
+    #path('rimuovi_conto/<string:valuta_type>', views.rimuovi_conto, name='rimuovi_conto'),
+    path('converti_valute/', views.convertitore, name='converti_valute'),
+    #path('selezione_cambio/', views.modifica_cambio_dashboard, nome='selezione_cambio')
 ]
