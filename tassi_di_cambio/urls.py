e899fac0177a7f0d24364f80520f788094435908
giacoballoccu <giacoballoccu@live.it>
2019-06-25 18:46:31 +0200

    Possibilità di sincronizzare i rate una volta al giorno inserita

--- tassi_di_cambio/urls.py
+++ tassi_di_cambio/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.convertitore, name='converti_valute'),
     path('ajax/get_rates/', views.get_rates, name='get_rates'),
+    path('/ajax/update_rates/', views.aggiorna_coin_rates, name='update_rates'),
 ]
