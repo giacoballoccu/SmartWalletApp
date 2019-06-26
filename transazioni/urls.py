491beb2aa413891063566651f661acc3c623faba
GiuliaAriu <giuulia.ariu@gmail.com>
2019-06-26 12:48:06 +0200

    Map url dettaglio transazione

--- transazioni/urls.py
+++ transazioni/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='transazioni'),
     path('nuova_transazione/', views.crea_transazione, name='nuova_transazione'),
-    #path('dettaglio/<string:transazione_id>/', views.dettaglio_transazione, name='dettaglio_transazione'),
+    path('dettaglio/(?P<id>\d+)/', views.dettaglio_transazione, name='dettaglio_transazione'),
 ]
