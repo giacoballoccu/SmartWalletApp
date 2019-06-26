ef62378c5c765d4647ffd16f81db8dd26e2f3ee8
GiuliaAriu <giuulia.ariu@gmail.com>
2019-06-26 12:30:34 +0200

    Map url elimina conto

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('aggiungi_conto/', views.aggiungi_conto, name='aggiungi_conto'),
-    #path('modifica_conto/<string:valuta_type>/', views.modifica_importo, name='modifica_importo_conto'),
-    #path('rimuovi_conto/<string:valuta_type>', views.rimuovi_conto, name='rimuovi_conto'),
+    path('rimuovi_conto/(?P<id>\d+)/', views.rimuovi_conto, name='elimina_conto'),
     #path('selezione_cambio/', views.modifica_cambio_dashboard, nome='selezione_cambio')
 ]
